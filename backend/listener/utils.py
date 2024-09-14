e36fae205dbeac8d5912a540479c2289979b1194
Alex Zhao <alexzhao.mailbox@gmail.com>
2024-09-14 19:00:23 -0400

    more db pushing updates

--- backend/listener/utils.py
+++ backend/listener/utils.py
@@ -10,13 +10,10 @@ load_dotenv()
 
 username = os.getenv('MIT_USERNAME')
 password = os.getenv('MIT_PASSWORD')
-url = 'https://mailman.mit.edu/mailman/private/reuse/2024-September.txt.gz'
-login_url = 'https://mailman.mit.edu/mailman/private/reuse/'
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 # Create OpenAI client
 client = OpenAI(api_key=openai_api_key)
-
 
 supabase_url: str = os.environ.get("SUPABASE_URL")
 supabase_key: str = os.environ.get("SUPABASE_SERVICE_KEY")
@@ -47,7 +44,7 @@ class Email:
         }
 
 
-def get_logs() -> str:
+def get_logs(login_url, url) -> str:
     # Initialize a session to maintain cookies and session state
     session = requests.Session()
 
@@ -81,18 +78,16 @@ def get_logs() -> str:
     return None
 
 
-def parse_body(email_body: str) -> str:
+def get_location(email_body: str) -> str:
     try:
         # Prepare the message for ChatGPT
-        system_prompt_1 = """You are a helpful assistant that extracts drop-off locations from email bodies. Do not include any other text in the response. Try several times to extract the drop-off location from the email body. If the location is not found, return "Location not found".
+        system_prompt_1 = """You are a helpful assistant that extracts pick-up locations from email bodies. Do not include any other text in the response. If the location is not found, return "Location not found".
         
-        All dropoffs happen in or around the MIT campus.
-        
-        Remember: If the location is not found, return "Location not found"."""
+        All pickups happen in or around MIT campus. Also includes Boston, Harvard."""
 
         messages = [
             {"role": "system", "content": system_prompt_1},
-            {"role": "user", "content": f"What is the drop-off location mentioned in this email body?\n\n{email_body}"}
+            {"role": "user", "content": f"What is the pick-up location mentioned in this email body?\n\n{email_body}"}
         ]
 
         # Make the API call to ChatGPT
@@ -135,7 +130,79 @@ def parse_body(email_body: str) -> str:
         return "Location not found"
 
 
+def check_img_url(url):
+    if url.startswith(('http://mailman.mit.edu', 'https://mailman.mit.edu')):
+        # Check if the URL ends with 'htm' or 'html'
+        if url.lower().endswith(('htm', 'html', 'htm>')):
+            return False
+        return True
+    if url.startswith('http://aka.ms') or url.startswith('https://aka.ms'):
+        return False
+    return True
+
+
+def get_name_addr_from_line(line):
+    email_from = line[5:].strip()
+    sender_name = ""
+    if "mit" in email_from.lower():
+        # Check for parentheses and extract name if present
+        if '(' in email_from and ')' in email_from:
+            name_start = email_from.index('(') + 1
+            name_end = email_from.index(')')
+            sender_name = email_from[name_start:name_end].strip()
+            # Remove parentheses and name
+            email_from = email_from[:name_start-1].strip()
+
+        if email_from.startswith('Reuse'):
+            email_from = 'Reuse'
+        # Replace 'at' with '@' in email address
+        if ' at ' in email_from:
+            username, domain = email_from.split(' at ')
+            email_from = f"{username}@{domain}"
+
+    else:
+        # Ask GPT-4 to fetch the name and email address
+        prompt = f"Extract the name and email address from this string: '{email_from}'. Format the response as 'Name: [name], Email: [email]'"
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "user", "content": prompt}]
+        )
+        result = response.choices[0].message.content
+        name_match = re.search(r'Name: (.+),', result)
+        email_match = re.search(r'Email: (.+)', result)
+        if name_match and email_match:
+            sender_name = name_match.group(1).strip()
+            email_from = email_match.group(1).strip()
+    return email_from, sender_name
+
+
+def get_subj_from_line(line):
+    subject = line[8:].strip()
+    if subject.startswith('[Reuse]'):
+        email_subject = subject[7:].strip()
+    elif subject.startswith('[Free-food]'):
+        email_subject = subject[11:].strip()
+    else:
+        email_subject = subject
+    return email_subject
+
+
+def is_opportunity(subject, body):
+    return True
+    prompt = f"Check if this email is confirming that something is claimed. If it is saying something is claimed, return 'False'. Otherwise if it is describing or providing something new, return 'True'. Here is the email: Subject: {subject} Body: {body} Is this offering or describing something? Format the response as 'True' or 'False'."
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    result = response.choices[0].message.content
+    if result.lower() == "false":
+        print("Skipping non-opportunity email: ", body)
+        return False
+    return True
+
+
 def parse_logs(log_text: str) -> List[Email]:
+    # Split the log text into individual emails
     emails = []
     current_email = []
     inside_digest = False
@@ -164,103 +231,109 @@ def parse_logs(log_text: str) -> List[Email]:
         emails.append('\n'.join(current_email))
 
     parsed_emails = []
-    for email_text in emails[:15]:
+    for email_text in emails[-30:]:
         sender_name = ""
         lines = email_text.split('\n')
         body_lines = []
         in_body = False
+        exists = False
+        is_useless = False
+
+        attachment_links = []
+        in_attachments = False
+
+        # Regular expression pattern to match URLs
+        url_pattern = re.compile(
+            r'https?://(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)'
+        )
 
         for line in lines:
+            if is_useless or exists:
+                break
             if line.startswith('From:'):
-                email_from = line[5:].strip()
-                if "mit" in email_from.lower():
-                    # Check for parentheses and extract name if present
-                    if '(' in email_from and ')' in email_from:
-                        name_start = email_from.index('(') + 1
-                        name_end = email_from.index(')')
-                        sender_name = email_from[name_start:name_end].strip()
-                        # Remove parentheses and name
-                        email_from = email_from[:name_start-1].strip()
-
-                    if email_from.startswith('Reuse'):
-                        email_from = 'Reuse'
-                    # Replace 'at' with '@' in email address
-                    if ' at ' in email_from:
-                        username, domain = email_from.split(' at ')
-                        email_from = f"{username}@{domain}"
-
-                else:
-                    # Ask GPT-4 to fetch the name and email address
-                    prompt = f"Extract the name and email address from this string: '{email_from}'. Format the response as 'Name: [name], Email: [email]'"
-                    response = client.chat.completions.create(
-                        model="gpt-4o",
-                        messages=[{"role": "user", "content": prompt}]
-                    )
-                    result = response.choices[0].message.content
-                    name_match = re.search(r'Name: (.+),', result)
-                    email_match = re.search(r'Email: (.+)', result)
-                    if name_match and email_match:
-                        sender_name = name_match.group(1).strip()
-                        email_from = email_match.group(1).strip()
+                email_from, sender_name = get_name_addr_from_line(line)
 
             elif line.startswith('Date:'):
                 email_date = line[5:].strip()
+
             elif line.startswith('Subject:'):
-                subject = line[8:].strip()
-                if subject.startswith('[Reuse]'):
-                    email_subject = subject[7:].strip()
-                else:
-                    email_subject = subject
+                email_subject = get_subj_from_line(line)
+                if email_subject.startswith(('Reuse Digest', 'Fwd:', 'Re:')):
+                    is_useless = True
+                    break
+                if check_existence(email_date, email_subject):
+                    exists = False
             elif line.startswith('Message-ID:'):
                 continue
+            elif line.startswith('In-Reply-To:'):
+                is_useless = True
+                break
             elif not line.strip() and not in_body:
                 in_body = True
             elif in_body:
+                if line == "-------------- next part --------------" or line.endswith("attachment was scrubbed...") or line.startswith("Get Outlook for"):
+                    in_body = False
+                    in_attachments = True
+                    continue
                 body_lines.append(line)
+            elif in_attachments:
+                attachment_links += [link for link in url_pattern.findall(
+                    line) if check_img_url(link)]
 
         # Post-processing
-        if email_subject.startswith('Reuse Digest'):
+        if is_useless or exists:
             continue
 
         email_body = '\n'.join(body_lines).strip()
 
-        prompt = f"Check if this email is actually offering a reuse opportunity, i.e. is giving something out for free. If it is confirming that something is claimed or is providing additional information about what is available, it is NOT a reuse opportunity. If it is an opportunity, return 'True'. If not an opportunity, return 'False'. Here is the email: {email_body} Is this a reuse opportunity? Format the response as 'True' or 'False'."
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=[{"role": "user", "content": prompt}]
-        )
-        result = response.choices[0].message.content
-        if result.lower() == "false":
-            print("Skipping non-opportunity email: ", email_body)
+        if not is_opportunity(email_subject, email_body):
             continue
 
         email = Email(email_subject, email_from,
                       email_date, email_body, "No known location", sender_name)
 
-        # Regular expression pattern to match URLs
-        url_pattern = re.compile(
-            r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-
         # Find all links in the email body, excluding specific domains
         email.links = [
             link for link in url_pattern.findall(email_body)
-            if not link.startswith(('http://mailman.mit.edu', 'https://mailman.mit.edu', 'http://aka.ms', 'https://aka.ms'))
-        ]
+            if check_img_url(link)
+        ] + attachment_links
 
-        # Remove text after and including the separator if present in the email body
-        separator = "________________________________"
-        if separator in email.body:
-            email.body = email.body.split(separator)[0].strip()
+        # # Remove text after and including the separator if present in the email body
+        # separator = "________________________________"
+        # if separator in email.body:
+        #     email.body = email.body.split(separator)[0].strip()
 
         parsed_emails.append(email)
 
-    emails = parsed_emails
+    emails = []
+    for email in parsed_emails:
+        is_duplicate = False
+        for other_email in parsed_emails:
+            if email.subject == other_email.subject and email.date > other_email.date:
+                is_duplicate = True
+                break
+        if not is_duplicate:
+            emails.append(email)
 
     return emails
 
 
+def check_existence(date, subject):
+    # Check if a row with the same timestamp and same name already exists in the database
+    result = supabase.table("items").select(
+        "*").eq("created_at", date).eq("name", subject).execute()
+
+    # If any rows are returned, the email already exists
+    if result.data:
+        print(
+            f"Email with subject '{subject}' from {date} already exists in the database.")
+        return True
+
+    return False
+
+
 def write_to_db(email: Email):
-    email_location = parse_body(email.body)
+    email_location = get_location(email.subject + "\n" + email.body)
     email.location = email_location
     email_json = email.to_json()
     print(email_json)
@@ -268,9 +341,11 @@ def write_to_db(email: Email):
 
 
 if __name__ == "__main__":
+    url = 'https://mailman.mit.edu/mailman/private/reuse/2024-September.txt.gz'
+    login_url = 'https://mailman.mit.edu/mailman/private/reuse/'
     supabase.table("items").delete().neq(
         'id', '00000000-0000-0000-0000-000000000000').execute()
-    emails = parse_logs(get_logs())
+    emails = parse_logs(get_logs(login_url, url))
     for email in emails[:1]:
         print(email)
         print("———————————————————————————————————————————————————————————")
