ddbe07728ff7c30b658608ebb8989e1b00e8ffd7
Juni Kim <junikimm717@gmail.com>
2024-09-14 12:23:27 -0400

    nav.tsx and vite.config.ts updated

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className="w-full flex flex-row justify-between">
+      <div>
+        {/* Pages here? */}
+      </div>
+      <div>
+        {/* Profile here? */}
+      </div>
+    </div>
+  )
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+  build: {
+    outDir: "../dist",
+  },
 })
