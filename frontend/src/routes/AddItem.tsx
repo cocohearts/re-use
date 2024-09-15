796946ab24c2059cda33f777e402553d0859739d
William Y. Feng <womogenes2@gmail.com>
2024-09-15 00:31:44 -0400

    Add photo updating UI

--- frontend/src/routes/AddItem.tsx
+++ frontend/src/routes/AddItem.tsx
@@ -10,24 +10,80 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
-import { Plus } from 'lucide-react';
+import { Plus, X } from 'lucide-react';
+import { useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function AddItemPage() {
+  const photoUploadRef: any = useRef(null);
+  const [photos, setPhotos] = useState<{ src: string; id: string }[]>([]);
+
+  const onPhotoUpload = (e: any) => {
+    const { files }: { files: File[] } = e.target;
+    if (files.length === 0) return;
+
+    Array.from(files).forEach((file: File) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (!e || !e.target) return;
+        const id = uuidv4();
+        setPhotos((photos) => [
+          ...photos,
+          { src: e.target?.result as string, id },
+        ]);
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const removePhoto = (id: string) => {
+    setPhotos((photos) => photos.filter((photo) => photo.id !== id));
+  };
+
   return (
     <div className="text-pine-900">
-      <h1 className="text-3xl">list a free item</h1>
+      <h1 className="mb-4 text-3xl">list an item</h1>
 
       {/* Photos */}
-      <div className="mb-4 flex flex-col">
-        <span className="mb-4">photos &bull; 1/10</span>
+      <span className="">photos &bull; {photos.length}/10</span>
+      <div className="mb-4 mt-1.5 flex flex-col overflow-x-auto">
         <div className="flex h-40 gap-4">
           {/* All the photos in an array */}
-          <Button className="flex h-full w-40 flex-col gap-1" variant="default">
+          {photos.map((photo) => {
+            return (
+              <div
+                className="relative h-full w-40 shrink-0 overflow-hidden rounded-md"
+                key={photo.id}
+              >
+                <button
+                  className="absolute right-2 top-2 rounded-full bg-neutral-900/30"
+                  onClick={() => removePhoto(photo.id)}
+                >
+                  <X className="stroke-white p-0.5" />
+                </button>
+                <img className="h-full object-cover" src={photo.src} />
+              </div>
+            );
+          })}
+          <Button
+            className="flex h-full w-40 shrink-0 flex-col gap-1"
+            variant="default"
+            onClick={() => photoUploadRef?.current?.click()}
+          >
             <Plus />
             <span>add photo</span>
           </Button>
         </div>
       </div>
+
+      {/* Invisible photo upload element */}
+      <input
+        className="hidden"
+        type="file"
+        ref={photoUploadRef}
+        onChange={onPhotoUpload}
+        multiple
+      />
 
       {/* Form */}
       <div className="mb-8 flex flex-col gap-4">
@@ -54,6 +110,7 @@ export default function AddItemPage() {
               <SelectItem value="like new">like new</SelectItem>
               <SelectItem value="good">good</SelectItem>
               <SelectItem value="bad">decent</SelectItem>
+              <SelectItem value="unspecified">unspecified</SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -81,7 +138,7 @@ export default function AddItemPage() {
         </div>
         {/* Tags */}
         <div>
-          <Label htmlFor="tags-input">tags</Label>
+          <Label htmlFor="tags-input">tags (optional)</Label>
           <p className="text-sm opacity-80">a comma-separated list of tags</p>
           <Input
             className="mt-1"
@@ -92,7 +149,7 @@ export default function AddItemPage() {
           />
         </div>
         {/* Self-pickup */}
-        <div>
+        {/* <div>
           <Label htmlFor="self-pickup-input">self-pickup *</Label>
           <p className="text-sm opacity-80">
             can people pick up items without notifying you?
@@ -107,7 +164,7 @@ export default function AddItemPage() {
               <Label htmlFor="option-no">no, i will choose the bidder</Label>
             </div>
           </RadioGroup>
-        </div>
+        </div> */}
       </div>
 
       {/* Submit button */}
