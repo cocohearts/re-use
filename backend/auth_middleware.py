25d1d621ce76bb8ff045b967515c1e36c7b52ad3
William Y. Feng <womogenes2@gmail.com>
2024-09-14 20:25:02 -0400

    Half-implement auth middleware

---
+++ backend/auth_middleware.py
@@ -0,0 +1,54 @@
+import time
+import jwt
+import os
+from dotenv import load_dotenv
+
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from backend.supabase import supabase
+
+load_dotenv()
+
+SUPABASE_JWT_SECRET = os.getenv("SUPABASE_JWT_SECRET")
+
+app = FastAPI()
+
+def verify_access_token(request):
+    authorization = request.headers.get("Authorization")
+
+    if not authorization:
+        return None
+
+    access_token = authorization.split(" ")[1]
+
+    if access_token:
+        payload = jwt.decode(
+            access_token,
+            key=SUPABASE_JWT_SECRET,
+            do_verify=True,
+            algorithms=["HS256"],
+            audience="authenticated",
+        )
+        return payload
+    
+    return None
+    
+
+# https://medium.com/@chandanp20k/leveraging-custom-middleware-in-python-fastapi-for-enhanced-web-development-09ba72b5ddc6
+class AuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        try:
+            # Call the verify_access_token function to validate the token
+            user = verify_access_token(request)
+
+            # If token validation succeeds, continue to the next middleware or route handler
+            request.state.user = user
+
+            response = await call_next(request)
+            return response
+        
+        except Exception as e:
+            # If token validation fails due to other exceptions, return a generic error response
+            return JSONResponse(content={"detail": f"Error: {str(e)}"}, status_code=500)
--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,14 +1,29 @@
 import { useAuthContext } from '@/components/AuthProvider';
+import { useEffect } from 'react';
 
 export default function ProfilePage() {
-  const { token: authToken } = useAuthContext();
-  console.log('authToken:', authToken);
+  const { token: authToken, user } = useAuthContext();
 
   // Pull profile data from server
+  useEffect(() => {
+    if (!user) return;
+
+    (async () => {
+      console.log('running async fetch...');
+      const res = await fetch(`/api/get-user/${user.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      console.log('result:', res);
+    })();
+  }, [user]);
 
   return (
     <div className="px-4">
-      <h1>Hello world</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 }
